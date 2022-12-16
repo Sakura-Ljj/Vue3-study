7885c8b0d0e1cc601dbd2f3d449d61ccc60022f3
TENCENT\v_jnnjieluo <v_jnnjieluo@tencent.com>
2022-12-16 19:21:17 +0800

    Vue3实现评分组件, 应用defineProps与defineEmits, 上传学习资料

---
+++ src/components/rate.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @Date: 2022-12-16 11:28:05
+ * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @LastEditTime: 2022-12-16 16:12:25
+ * @FilePath: \vue3project\src\components\rate.vue
+ * @Description: 
+-->
+<template>
+    <div :class=themeObj[theme]>
+        <div class="tw-relative tw-inline-block tw-text-lg">
+            <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
+            <span class="tw-absolute tw-inline-block tw-inset-0 tw-overflow-hidden" :style="fontWidth">
+                <span @mouseover="mouseOver(num)" @click="onRate(num)" v-for="num in 5" :key="num">★</span>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineProps, defineEmits, computed } from 'vue';
+
+const themeObj = {
+    red: 'tw-text-red',
+    orange: 'tw-text-orange-500',
+    green: 'tw-text-green-700',
+}
+
+const { score, theme } = defineProps({ 
+    score: Number,
+    theme: {type: String, default: 'red'}
+})
+
+const emits = defineEmits('updateRate')
+
+const width = ref(score)
+
+const mouseOver = num => {
+    width.value = num
+}
+
+const onRate = num => {
+    emits('updateRate', num)
+}
+
+const fontWidth = computed(() => `width: calc(16px * ${width.value});`)
+
+
+
+
+// const rate = computed(() => '★★★★★☆☆☆☆☆'.slice(5 - score, 10 - score))
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,7 +2,7 @@
  * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
  * @Date: 2022-12-14 16:10:25
  * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
- * @LastEditTime: 2022-12-14 17:37:45
+ * @LastEditTime: 2022-12-16 14:21:01
  * @FilePath: \vue3project\src\pages\Home.vue
  * @Description: 
 -->
@@ -10,7 +10,16 @@
 <template>
     <h1>This is Home</h1>
     <Todolist />
+    <Rate :score="score" theme="orange" @updateRate="updateRate"/>
 </template>
 <script setup>
-  import Todolist from '../components/todolist.vue';
+import { ref } from 'vue';
+import Todolist from '../components/todolist.vue';
+import Rate from '../components/rate.vue';
+
+const score = ref(3)
+
+const updateRate = num => {
+    score.value = num
+}
 </script>
