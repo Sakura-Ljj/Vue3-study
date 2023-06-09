f5099140b4f011e2b8f509952048dc3f3d665936
TENCENT\v_jnnjieluo <v_jnnjieluo@tencent.com>
2023-06-09 19:21:46 +0800

    补充注释

--- server/contorller/userContorller.js
+++ server/contorller/userContorller.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @Date: 2023-06-09 17:33:44
+ * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @LastEditTime: 2023-06-09 19:16:13
+ * @FilePath: \vue3project\server\contorller\userContorller.js
+ * @Description: 用户相关 CGI
+ */
 function userLoginApi (event, req, res) {
     console.log(event)
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @Date: 2023-06-09 17:16:07
+ * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @LastEditTime: 2023-06-09 19:20:09
+ * @FilePath: \vue3project\server\routes\index.js
+ * @Description: 所有路由配置集合
+ */
 const userRoutes = require('./userRoutes')
 
 module.exports = [...userRoutes]
