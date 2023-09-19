f7d233191fab5d7b64c65c9d92abb31713ced736
TENCENT\v_jnnjieluo <v_jnnjieluo@tencent.com>
2023-09-19 17:59:27 +0800

    面向移动端的适配粗略修改

---
+++ src/components/NavBar.vue
@@ -0,0 +1,159 @@
+<!--
+ * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @Date: 2023-09-18 14:50:05
+ * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
+ * @LastEditTime: 2023-09-19 17:55:01
+ * @FilePath: \vue3project\src\components\Navbar.vue
+ * @Description: 导航栏组件
+-->
+<template>
+    <div v-show="clientWidth >= 820" class="header tw-mb-3">
+        <div class="tw-max-w-full tw-flex tw-justify-around">
+            <div class="logo tw-text-blue-400 tw-py-2">LOGO</div>
+            <ul class="tw-flex">
+                <li
+                    v-for="item in props.navBar"
+                    :key="item.path"
+                    :class="['nav-item', route.path === item.path ? 'curr-nav' : '']"
+                    @click="toPath(item.path)"
+                >
+                    {{item.text}}
+                </li>
+            </ul>
+            <div v-if="!props.loading" class="tw-flex tw-items-center">
+                <div v-if="user.userInfo.userId">
+                    <el-dropdown>
+                        <div class="tw-flex tw-cursor-pointer tw-items-center user-info">
+                            <el-avatar :src="user.userInfo.avatar ? `/api/common/getImage?url=${user.userInfo.avatar}` : ''" class="tw-mr-2">
+                                <el-icon size="30"><UserFilled /></el-icon>
+                            </el-avatar>
+                            <div>{{user.userInfo.nickName || 'NickName'}}</div>
+                        </div>
+                        <template #dropdown>
+                            <el-dropdown-menu>
+                                <el-dropdown-item icon="User" @click="toEditUserInfo">查看个人信息</el-dropdown-item>
+                                <el-dropdown-item icon="Lock">更改密码</el-dropdown-item>
+                                <el-dropdown-item divided @click="userLogout">退出登录</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </template>
+                    </el-dropdown>
+                </div>
+                <el-button v-else type="primary" @click="toLogin">登录</el-button>
+            </div>
+        </div>
+    </div>
+    <div v-show="clientWidth < 820">
+        <div class="tw-py-2 tw-bg-blue-400 tw-pl-2 tw-flex tw-items-center tw-justify-center tw-relative" @click="drawer = !drawer">
+            <el-icon color="#FFFFFF" size="30" class="mobile-drawer-icon"><Menu /></el-icon>
+            <span class="tw-text-lg tw-text-white">IS模拟官网</span>
+        </div>
+        <el-drawer
+            v-model="drawer"
+            direction="ltr"
+            size="65%"
+        >
+            <ul>
+                <li
+                    v-for="item in props.navBar"
+                    :key="item.path"
+                    :class="['mobile-nav-item', route.path === item.path ? 'mobile-curr-nav' : '']"
+                    @click="toPath(item.path)"
+                >
+                    <span>{{item.text}}</span>
+                </li>
+            </ul>
+        </el-drawer>
+    </div>
+</template>
+<script setup>
+import { defineProps, ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { useUserStore } from '../store/userStore'
+import { ElMessageBox, ElMessage } from 'element-plus'
+
+const router = useRouter()
+const route = useRoute()
+const user = useUserStore()
+const props = defineProps({
+    loading: { type: Boolean, default: false },
+    navBar: { type: Array, default: () => [] }
+})
+
+const clientWidth = ref(document.body.clientWidth)
+window.addEventListener('resize', () => {
+    clientWidth.value = document.body.clientWidth
+})
+
+const drawer = ref(false)
+
+const toPath = (path) => {
+    router.push(path)
+    drawer.value = false
+}
+
+const toLogin = () => {
+    router.push('/user/login')
+}
+
+const toEditUserInfo = () => {
+    router.push('/user/info')
+}
+
+const userLogout = async () => {
+    try {
+        await ElMessageBox.confirm('确认退出登录?', '提示', {
+            confirmButtonText: '确认',
+            cancelButtonText: '取消'
+        })
+        await user.userLogout()
+        ElMessage.success('成功退出')
+    } catch (e) {
+        ElMessage.info('成功取消')
+    }
+}
+</script>
+<style scoped>
+.logo {
+  width: 50px;
+  height:50px;
+  line-height: 50px;
+  cursor: pointer;
+}
+
+.header {
+  border-bottom: 1px solid #DDE6ED;
+  position: sticky;
+  top: 0;
+  box-shadow: 0 1px 0 0 rgba(0,0,0,.1);
+  background: #fff;
+  opacity: .95;
+  z-index: 999;
+}
+
+.nav-item {
+  @apply tw-mr-5 tw-cursor-pointer hover:tw-text-blue-400 tw-flex tw-items-center;
+}
+
+.curr-nav {
+  @apply tw-text-blue-400;
+  border-bottom: 2px solid;
+}
+
+.mobile-nav-item {
+  @apply tw-py-3 tw-rounded tw-pl-3
+}
+
+.mobile-curr-nav {
+  @apply tw-bg-blue-400 tw-bg-opacity-50 tw-text-white
+}
+
+.mobile-drawer-icon {
+  position: absolute !important;
+  left: 0.5rem !important
+}
+
+/* element plus 的 dropdown有bug focus-visible的时候会显示一个黑色的外边框 */
+.user-info:focus-visible {
+outline: unset;
+}
+</style>
--- src/pages/routerView.vue
+++ src/pages/routerView.vue
@@ -2,160 +2,60 @@
  * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
  * @Date: 2022-12-14 15:49:46
  * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
- * @LastEditTime: 2023-08-30 15:05:54
+ * @LastEditTime: 2023-09-18 17:29:47
  * @FilePath: \vue3project\src\pages\routerView.vue
  * @Description:
 -->
 <template>
-    <div v-if="pageData.navBar.some(item => item.path === route.path)">
-        <div class="header tw-mb-3">
-            <div class="tw-max-w-full tw-flex tw-justify-between tw-mx-60">
-                <div class="logo tw-text-blue-400 tw-py-2">LOGO</div>
-                <ul class="tw-flex">
-                    <li
-                        v-for="item in pageData.navBar"
-                        :key="item.path"
-                        :class="['nav-item', route.path === item.path ? 'curr-nav' : '']"
-                        @click="toPath(item.path)">
-                        {{item.text}}
-                    </li>
-                </ul>
-                <div v-if="!pageData.loading" class="tw-flex tw-items-center">
-                    <div v-if="user.userInfo.userId">
-                        <el-dropdown>
-                            <div class="tw-flex tw-cursor-pointer tw-items-center user-info">
-                                <el-avatar :src="user.userInfo.avatar ? `/api/common/getImage?url=${user.userInfo.avatar}` : ''" class="tw-mr-2">
-                                    <el-icon size="30"><UserFilled /></el-icon>
-                                </el-avatar>
-                                <div>{{user.userInfo.nickName || 'NickName'}}</div>
-                            </div>
-                            <template #dropdown>
-                                <el-dropdown-menu>
-                                    <el-dropdown-item icon="User" @click="toEditUserInfo">查看个人信息</el-dropdown-item>
-                                    <el-dropdown-item icon="Lock">更改密码</el-dropdown-item>
-                                    <el-dropdown-item divided @click="userLogout">退出登录</el-dropdown-item>
-                                </el-dropdown-menu>
-                            </template>
-                        </el-dropdown>
-                    </div>
-                    <el-button v-else type="primary" @click="toLogin">登录</el-button>
-                </div>
-            </div>
-        </div>
-        <div v-if="route.path.includes('indexPage')" class="carousel">
-            <Carousel />
-        </div>
-        <div class="main">
+    <div v-if="navBar.some(item => item.path === route.path)">
+        <NavBar :loading="loading" :nav-bar="navBar" />
+
+        <Carousel v-if="route.path.includes('indexPage')" />
+
+        <div class="tw-container tw-m-auto">
             <router-view :key="route.fullPath" />
         </div>
     </div>
     <router-view v-else :key="route.fullPath" />
 </template>
 <script setup>
-import { reactive, onMounted } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
-import Carousel from '../components/Carousel.vue'
+import { onMounted, ref } from 'vue'
 import { useUserStore } from '../store/userStore'
-import { ElMessageBox, ElMessage } from 'element-plus'
+import { useRoute } from 'vue-router'
+import Carousel from '../components/Carousel.vue'
+import NavBar from '../components/Navbar.vue'
 
-const router = useRouter()
-const route = useRoute()
 const user = useUserStore()
-const pageData = reactive({
-    navBar: [
-        {
-            text: '响应式数据及Vue3基础学习',
-            path: '/practice/vue3BasicPractice'
-        },
-        {
-            text: 'Vuex学习',
-            path: '/practice/vuexPractice'
-        },
-        {
-            text: 'Vue中的JSX',
-            path: '/practice/vuePracticeJSX'
-        },
-        {
-            text: 'IS动漫社模拟官网',
-            path: '/index/indexPage'
-        }
-    ],
+const route = useRoute()
 
-    loading: false
-})
-
-const toPath = (path) => {
-    router.push(path)
-}
-
-const toLogin = () => {
-    router.push('/user/login')
-}
-
-const toEditUserInfo = () => {
-    router.push('/user/info')
-}
-
-const userLogout = async () => {
-    try {
-        await ElMessageBox.confirm('确认退出登录?', '提示', {
-            confirmButtonText: '确认',
-            cancelButtonText: '取消'
-        })
-        await user.userLogout()
-        ElMessage.success('成功退出')
-    } catch (e) {
-        ElMessage.info('成功取消')
+const navBar = ref([
+    {
+        text: '响应式数据及Vue3基础学习',
+        path: '/practice/vue3BasicPractice'
+    },
+    {
+        text: 'Vuex学习',
+        path: '/practice/vuexPractice'
+    },
+    {
+        text: 'Vue中的JSX',
+        path: '/practice/vuePracticeJSX'
+    },
+    {
+        text: 'IS动漫社模拟官网',
+        path: '/index/indexPage'
     }
-}
+])
+
+const loading = ref(false)
 
 onMounted(async () => {
-    pageData.loading = true
+    loading.value = true
     try {
         await user.getUserInfo()
-        pageData.loading = false
+        loading.value = false
     } catch (e) {
-        pageData.loading = false
+        loading.value = false
     }
 })
 </script>
-<style scoped>
-.logo {
-  width: 50px;
-  height:50px;
-  line-height: 50px;
-  cursor: pointer;
-}
-
-.header {
-  border-bottom: 1px solid #DDE6ED;
-  position: sticky;
-  top: 0;
-  box-shadow: 0 1px 0 0 rgba(0,0,0,.1);
-  background: #fff;
-  opacity: .95;
-  z-index: 999;
-}
-
-.nav-item {
-  @apply tw-mr-5 tw-cursor-pointer hover:tw-text-blue-400 tw-flex tw-items-center;
-}
-
-.curr-nav {
-  @apply tw-text-blue-400;
-  border-bottom: 2px solid;
-}
-
-.carousel {
-  @apply tw-m-auto;
-  width: 1851px;
-}
-.main {
-  @apply tw-container tw-m-auto;
-}
-
-/* element plus 的 dropdown有bug focus-visible的时候会显示一个黑色的外边框 */
-.user-info:focus-visible {
-outline: unset;
-}
-</style>
