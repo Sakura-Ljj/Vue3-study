e16ccd9ab450001ab3df0bfba0fafec7ee51c0cd
TENCENT\v_jnnjieluo <v_jnnjieluo@tencent.com>
2023-10-13 15:37:07 +0800

    开启打包压缩优化

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
  * @Date: 2022-12-14 15:49:46
  * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
- * @LastEditTime: 2023-10-13 15:22:53
+ * @LastEditTime: 2023-10-13 15:33:57
  * @FilePath: \vue3project\vite.config.js
  * @Description: 构建配置
  */
@@ -10,8 +10,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJSX from '@vitejs/plugin-vue-jsx'
 import path from 'path'
-// import CompressionVitePlugin from 'vite-plugin-compression'
-// import imageminVitePlugin from 'vite-plugin-imagemin'
+import CompressionVitePlugin from 'vite-plugin-compression'
+import imageminVitePlugin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -44,43 +44,43 @@ export default defineConfig({
     plugins: [
         vue(),
 
-        vueJSX()
+        vueJSX(),
 
-        // new CompressionVitePlugin({
-        //     filename: '[path][base].gz',
-        //     algorithm: 'gzip',
-        //     test: /.js$|.css$|.html$/,
-        //     threshold: 10240, // 对超过10k的数据压缩
-        //     minRatio: 0.8 // 压缩率小于0.8才会压缩
-        // }),
+        new CompressionVitePlugin({
+            filename: '[path][base].gz',
+            algorithm: 'gzip',
+            test: /.js$|.css$|.html$/,
+            threshold: 10240, // 对超过10k的数据压缩
+            minRatio: 0.8 // 压缩率小于0.8才会压缩
+        }),
 
-        // imageminVitePlugin({
-        //     gifsicle: {
-        //         optimizationLevel: 7,
-        //         interlaced: false
-        //     },
-        //     optipng: {
-        //         optimizationLevel: 7
-        //     },
-        //     mozjpeg: {
-        //         quality: 20
-        //     },
-        //     pngquant: {
-        //         quality: [0.8, 0.9],
-        //         speed: 4
-        //     },
-        //     svgo: {
-        //         plugins: [
-        //             {
-        //                 name: 'removeViewBox'
-        //             },
-        //             {
-        //                 name: 'removeEmptyAttrs',
-        //                 active: false
-        //             }
-        //         ]
-        //     }
-        // })
+        imageminVitePlugin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false
+            },
+            optipng: {
+                optimizationLevel: 7
+            },
+            mozjpeg: {
+                quality: 20
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox'
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false
+                    }
+                ]
+            }
+        })
     ],
 
     resolve: {
