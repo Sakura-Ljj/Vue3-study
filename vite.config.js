ccfc38d17dda5f178ec5c73d83453a9d02dc05ba
TENCENT\v_jnnjieluo <v_jnnjieluo@tencent.com>
2023-10-13 15:24:43 +0800

    接入打包压缩及图片压缩优化

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
  * @Date: 2022-12-14 15:49:46
  * @LastEditors: TENCENT\v_jnnjieluo v_jnnjieluo@tencent.com
- * @LastEditTime: 2023-08-28 15:09:58
+ * @LastEditTime: 2023-10-13 15:22:53
  * @FilePath: \vue3project\vite.config.js
  * @Description: 构建配置
  */
@@ -10,6 +10,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJSX from '@vitejs/plugin-vue-jsx'
 import path from 'path'
+// import CompressionVitePlugin from 'vite-plugin-compression'
+// import imageminVitePlugin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,7 +41,47 @@ export default defineConfig({
     //     cors: true // 配置 CORS
     // },
 
-    plugins: [vue(), vueJSX()],
+    plugins: [
+        vue(),
+
+        vueJSX()
+
+        // new CompressionVitePlugin({
+        //     filename: '[path][base].gz',
+        //     algorithm: 'gzip',
+        //     test: /.js$|.css$|.html$/,
+        //     threshold: 10240, // 对超过10k的数据压缩
+        //     minRatio: 0.8 // 压缩率小于0.8才会压缩
+        // }),
+
+        // imageminVitePlugin({
+        //     gifsicle: {
+        //         optimizationLevel: 7,
+        //         interlaced: false
+        //     },
+        //     optipng: {
+        //         optimizationLevel: 7
+        //     },
+        //     mozjpeg: {
+        //         quality: 20
+        //     },
+        //     pngquant: {
+        //         quality: [0.8, 0.9],
+        //         speed: 4
+        //     },
+        //     svgo: {
+        //         plugins: [
+        //             {
+        //                 name: 'removeViewBox'
+        //             },
+        //             {
+        //                 name: 'removeEmptyAttrs',
+        //                 active: false
+        //             }
+        //         ]
+        //     }
+        // })
+    ],
 
     resolve: {
         alias: {
